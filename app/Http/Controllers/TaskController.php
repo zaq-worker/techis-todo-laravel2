55d9cf97fb78ef7ce33c249c3be630a6a9c9d70e
zaq-worker <yasushi1002-s14@outlook.com>
2021-09-27 02:08:30 +0900

    TaskController 修正

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -76,6 +76,9 @@ class TaskController extends Controller
      */
     public function destroy(Request $request, Task $task)
     {
+
+        $this->authorize('destroy', $task);
+
         $task->delete();
         return redirect('/tasks');
     }
